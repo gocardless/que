362c6b6f351848f47d36d70334477000344a20b2
Lawrence Jones <lawrjone@gmail.com>
2018-04-09 15:29:49 +0100

    Refactor Que::Job, Que::Worker
    This commit modifies the Job and Worker class to better reflect the
    separation of responsibilities between the two concepts. In summary, the
    Worker class knows how to acquire and run jobs, while Job knows how to
    enqueue, run and destroy itself.
    
    One thing to note is the removal of any parallelism from the Que worker.
    We don't use this at GoCardless, and are making the decision to remove
    this temporarily. This will be added back in at a later point, but will
    be managed externally to the Worker code, as orchestrating workers is
    not a responsibility of a worker.
    
    -# Que::Job
    
    Prior to this change, the Job class included a logic about how to lock a
    job, which is (in our opinion) essential to the function of a worker. It
    also contained code for JSON serialization and Ruby constant lookups,
    which are best placed elsewhere.
    
    Now the interface for Que::Job comprises of enqueue, run and destroy.
    The assignment of default parameters and the handling of extracting
    those attributes from parameters handed to the job is simplified, while
    maintaining compatibility with the semantics of the previous Job
    implementation.
    
    -# Que::Worker
    
    Previously Worker contained code that tracked a grab-bag of Que module
    constants, and responded to class level methods by mutating global state
    to create many worker instances. The worker itself did not know how to
    lock a job, and defined several (global) class instance variables.
    
    We have now removed all code from Worker that wasn't directly related to
    finding and working jobs. The class is now exclusively about working a
    job, and repeating that ad infinitum. Instead of using global instance
    variables, each Worker is initialized with its configuration, removing
    an entire category of errors when tweaking Worker config variables.
    
    Co-authored-by: Harry Maclean <harrymaclean@gocardless.com>

---
+++ spec/lib/que/worker_spec.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Que::Worker do
+  describe ".work" do
+    subject { described_class.new.work }
+
+    it "works a job if there's one to work" do
+      FakeJob.enqueue(1)
+
+      expect(subject).to eq(:job_worked)
+      expect(FakeJob.log).to eq([1])
+      expect(QueJob.count).to eq(0)
+    end
+
+    it "returns job_not_found if there's no job to work" do
+      expect(subject).to eq(:job_not_found)
+    end
+
+    context "when a job raises an exception" do
+      it "rescues it" do
+        ExceptionalJob.enqueue(1)
+
+        expect(subject).to eq(:job_worked)
+      end
+
+      context "and the job has a failure handler" do
+        let(:job_class) { ExceptionalJob::WithFailureHandler }
+
+        it "calls it" do
+          job_class.enqueue(1)
+
+          expect(subject).to eq(:job_worked)
+
+          expect(job_class.log.count).to eq(2)
+          expect(job_class.log.first).to eq([:run, 1])
+
+          method, error, job = job_class.log.second
+
+          expect(method).to eq(:handle_job_failure)
+          expect(error.class).to eq(ExceptionalJob::Error)
+          expect(job).to be_a(Hash)
+        end
+      end
+
+      context "when the job doesn't have a failure handler" do
+        let(:job_class) { ExceptionalJob }
+
+        it "calls the default one" do
+          job_class.enqueue("foo")
+
+          expect(subject).to eq(:job_worked)
+
+          job = QueJob.first
+
+          expect(job.error_count).to eq(1)
+          expect(job.run_at).to be > postgres_now
+          expect(job.last_error).to match("bad argument foo")
+        end
+      end
+
+      context "when postgres raises an exception while processing a job" do
+        it "rescues it and returns an error" do
+          FakeJob.enqueue(1)
+
+          expect(Que).to receive(:execute).with(:lock_job, [""]).and_raise(PG::Error)
+          expect(subject).to eq(:error)
+        end
+      end
+    end
+  end
+end
